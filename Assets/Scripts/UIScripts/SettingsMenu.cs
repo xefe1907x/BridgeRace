1b564ee7f3e3412e8cba0e89c632946bcb644e51
xefe1907x <efeaslan1995@hotmail.com>
2022-10-25 19:10:06 +0300

    Added 2 panels to stage 0, sound settings is done.

---
+++ Assets/Scripts/UIScripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    
+    public Slider slider;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
